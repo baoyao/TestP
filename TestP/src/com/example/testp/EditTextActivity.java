f6924a5db5afdbf142b329d9d5c1a63a748bcd1c
baoyao <bao_yao@foxmail.com>
2016-09-22 20:43:04 +0800

    add calc

--- TestP/src/com/example/testp/EditTextActivity.java
+++ TestP/src/com/example/testp/EditTextActivity.java
@@ -1,12 +1,13 @@
 package com.example.testp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Selection;
 import android.text.Spannable;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -33,21 +34,14 @@ public class EditTextActivity extends Activity {
         String text = edit.getText().toString();
 
         switch (view.getId()) {
-        case R.id.btn0:
-            ea.insert(nowSelection, "0");
-            nowSelection += 1;
-            break;
-        case R.id.btn1:
-            ea.insert(nowSelection, "1");
-            nowSelection += 1;
-            break;
         case R.id.btn_del:
             if (nowSelection == 0) {
                 return;
             }
             text = edit.getText().toString();
 
-            boolean bool=(text.length() > 0 && " ".equals(text.substring(nowSelection - 1,nowSelection)));
+            boolean bool = (text.length() > 0 && " ".equals(text.substring(
+                    nowSelection - 1, nowSelection)));
             if (bool) {
                 ea.delete(nowSelection - 1, nowSelection);
                 nowSelection -= 1;
@@ -59,14 +53,17 @@ public class EditTextActivity extends Activity {
 
             ea.delete(nowSelection - 1, nowSelection);
             nowSelection -= 1;
-
+            break;
+        default:
+            ea.insert(nowSelection, ((Button) view).getText().toString());
+            nowSelection += 1;
             break;
         }
         text = edit.getText().toString();
 
-        int startSpace = text.length() / (spaceIndex+1);
+        int startSpace = text.length() / (spaceIndex + 1);
 
-        text=text.replaceAll(" ", "");
+        text = text.replaceAll(" ", "");
         // 添加空格 begin
         if (text.length() >= spaceIndex) {
             for (int i = 0; i < text.length() / spaceIndex; i++) {
@@ -75,35 +72,26 @@ public class EditTextActivity extends Activity {
                     break;
                 }
                 if (i == 0) {
-                    text = text.substring(0, spaceIndex) + " " + text.substring(spaceIndex);
+                    text = text.substring(0, spaceIndex) + " "
+                            + text.substring(spaceIndex);
                 } else {
-                    text = text.substring(0, subIndex) + text.substring(subIndex, subIndex + spaceIndex) + " "
-                            + text.substring(subIndex + spaceIndex);
+                    text = text.substring(0, subIndex + spaceIndex)
+                            + " " + text.substring(subIndex + spaceIndex);
                 }
             }
         }
         edit.setText(text);
         // 添加空格 end
 
-        int endSpace = text.length() / (spaceIndex+1);
-
-        switch (view.getId()) {
-        case R.id.btn0:
-            setSelectionIndex(edit, nowSelection+(endSpace - startSpace));
-            break;
-        case R.id.btn1:
-            setSelectionIndex(edit, nowSelection+(endSpace - startSpace));
-            break;
-        case R.id.btn_del:
-            setSelectionIndex(edit, nowSelection+(endSpace - startSpace));
-            break;
-        }
-
+        int endSpace = text.length() / (spaceIndex + 1);
+        
+        //移动光标
+        Selection.setSelection((Spannable) edit.getText(), nowSelection + (endSpace - startSpace));
     }
+    
 
-    private void setSelectionIndex(EditText edit, int index) {
-        Spannable spanText = (Spannable) edit.getText();
-        Selection.setSelection(spanText, index);
+    public void onButtonClick2(View view) {
+        startActivity(new Intent(this,MainActivity.class));
     }
 
 }
--- TestP/src/com/example/testp/MainActivity.java
+++ TestP/src/com/example/testp/MainActivity.java
@@ -255,7 +255,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-        int sId = (int) v.getTag();
+        int sId = (Integer) v.getTag();
         mSoundPool.play(sId, 1, 1, 0, 0, 1);
     }
 
